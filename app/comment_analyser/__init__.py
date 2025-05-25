caedc3ad8e2b51fddecb11193efe612e56c8a8b2
Horbee <n.horox@gmail.com>
2025-05-25 13:31:41 +0200

    Add Electra model

--- app/comment_analyser/__init__.py
+++ app/comment_analyser/__init__.py
@@ -1,9 +1,11 @@
 from .bert_cased_v4.handler import model_pipeline as bert_cased_v4_model_pipeline
-
+from .electra_uncased_downsampled.handler import model_pipeline as electra_uncased_downsampled_model_pipeline
 
 def comment_model_pipeline(comment: str):
     bert_cased_v4_probabilities = bert_cased_v4_model_pipeline(comment)
+    electra_uncased_downsampled_probabilities = electra_uncased_downsampled_model_pipeline(comment)
 
     return {
-        "bert_cased_v4_probabilities": bert_cased_v4_probabilities[0].cpu().tolist()
+        "bert_cased_v4_probabilities": bert_cased_v4_probabilities[0].cpu().tolist(),
+        "electra_uncased_downsampled_probabilities": electra_uncased_downsampled_probabilities[0].cpu().tolist()
     }
